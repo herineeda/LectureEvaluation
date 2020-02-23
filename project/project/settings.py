8a1f8d590954c6246f77c603cfa11947804274c4
eunohh <dmsdh316@naver.com>
2020-02-24 00:38:01 +0900

    - 검색 결과에서 강의평 리스트로 이동하는 버튼 완성

--- project/project/settings.py
+++ project/project/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.kakao',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
