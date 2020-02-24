791c6bc31a173a578aac04d1670eee4eca40a525
louren1234 <louren1234@naver.com>
2020-02-24 18:41:06 +0900

    존재하는 아이디라고 알려주는 페이지 추가

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import auth
 from .models import Profile
@@ -8,17 +8,26 @@ from .models import Profile
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(username=request.POST['email'], password=request.POST['password1'])
-            email = request.POST["email"]
-            major = request.POST["major"]
-            undergradNum = request.POST["undergradNum"]
-            studentname = request.POST.get('studentname', '')
-            #studentname = request.POST["studentname"]
-            profile = Profile(email=email, user=user, major=major, undergradNum=undergradNum, studentname=studentname)
-            profile.save()
-            auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend') #로그인 하는 함수
-            return redirect('home')
+            if request.POST['password1'] == request.POST['password2']:
+                email = request.POST["email"]
+                if User.objects.filter(username=email).exists():
+                    #return HttpResponse("이미 존재하는 이메일입니다.")
+                    return render(request, 'sameEmail.html')
+            # try:
+            #     if User.objects.filter(username=email).exists():
+            #         raise emailError
+            # except emailError:
+            #     return HttpResponse("이미 존재하는 이메일입니다.")
+                else:
+                    user = User.objects.create_user(username=request.POST['email'], password=request.POST['password1'])
+                    major = request.POST["major"]
+                    undergradNum = request.POST["undergradNum"]
+                    studentname = request.POST.get('studentname', '')
+                    #studentname = request.POST["studentname"]
+                    profile = Profile(email=email, user=user, major=major, undergradNum=undergradNum, studentname=studentname)
+                    profile.save()
+                    auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend') #로그인 하는 함수
+                    return redirect('home')
     return render(request, 'signup.html')  
 
 
